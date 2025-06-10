11856b60b59faaa89eb6d60670b15ec4487d8f51
Hiitsphil <philemonawork23@gmail.com>
2025-06-10 11:07:08 -0400

    3.1 done

--- fairyTale.js
+++ fairyTale.js
@@ -2,16 +2,16 @@
 let place="Dubai";
 
 // 2. Create a variable named `creatureName` and set the value to a string that says whatever name you want to give the creature.
-let creatureName="Dragon";
+let creatureName="Diddy";
 
 // 3. Create a variable named `color` and set the value to a string that says whatever color you want.
-let color="Purple";
+let color="purple";
 
 // 4. Create a variable named `color2` and set the value to a string that says a different color you want.
-let color2="Blue";
+let color2="blue";
 
 // 5. Create a variable named `emotion` and set the value to a string that says whatever emotion you want.
-let emotion="Happy";
+let emotion="happy";
 
 // 6. Create a variable named `character` and set the value to a string that says whatever name for a character you want.
 let character="Jedediah";
@@ -20,19 +20,23 @@ let character="Jedediah";
 let number="5";
 
 // 8. Create a variable named `item` and set the value to a string that says whatever item/thing you want.
-let item="Laptop";
+let item="minor";
 
 // 9. Create a variable named `item2` and set the value to a string that says a different item/thing you want.
-let item2="Mouse";
+let item2="bottle of baby oil";
 
 // 10. Create a variable named `verb` and set the value to a string that says whatever verb you want.
-let verb="Play";
+let verb="play";
 
 // 11. Create a variable named `adverb` and set the value to a string that says whatever adverb you want.
-let adverb="Ominously";
+let adverb="ominously";
 
 // 12. Create a variable named `number2` and set the value to a number greater than 1.
 let number2="9"
+
+let smell="bananas"
+
+let feeling2="hysterical"
 
 
 
@@ -43,19 +47,21 @@ let number2="9"
 // - Afterwards, run your code and press the button to see your story ✨
 
 document.querySelector("button").addEventListener("click", function() {
-    document.querySelector(".place").innerHTML = null;
-    document.querySelector(".creature-name").innerHTML = null;
-    document.querySelector(".color").innerHTML = null;
-    document.querySelector(".color-2").innerHTML = null;
-    document.querySelector(".emotion").innerHTML = null;
-    document.querySelector(".character").innerHTML = null;
-    document.querySelector(".creature-name-again").innerHTML = null;
-    document.querySelector(".number").innerHTML = null;
-    document.querySelector(".item").innerHTML = null;
-    document.querySelector(".character-again").innerHTML = null;
-    document.querySelector(".item-2").innerHTML = null;
-    document.querySelector(".verb").innerHTML = null;
-    document.querySelector(".adverb").innerHTML = null;
-    document.querySelector(".number-2").innerHTML = null;
+    document.querySelector(".place").innerHTML = place;
+    document.querySelector(".creature-name").innerHTML = creatureName;
+    document.querySelector(".color").innerHTML = color;
+    document.querySelector(".color-2").innerHTML = color2;
+    document.querySelector(".emotion").innerHTML = emotion;
+    document.querySelector(".character").innerHTML = character;
+    document.querySelector(".creature-name-again").innerHTML = creatureName;
+    document.querySelector(".number").innerHTML = number;
+    document.querySelector(".item").innerHTML = item;
+    document.querySelector(".character-again").innerHTML = character;
+    document.querySelector(".item-2").innerHTML = item2;
+    document.querySelector(".verb").innerHTML = verb;
+    document.querySelector(".adverb").innerHTML = adverb;
+    document.querySelector(".number-2").innerHTML = number2;
     document.querySelector(".story-container").style = "display:block";
+    document.querySelector("#smell").innerHTML= smell;
+    document.querySelector("#feeling2").innerHTML= feeling2;
 });
